eea4d1a75d791561dd26422c390a148db605e17b
Jordan Heiman <jordan.heiman@umontana.edu>
2024-10-17 10:05:52 -0600

    formatting package loading script for GitHub

--- 2.Code/2.Functions/20_package_loading.R
+++ 2.Code/2.Functions/20_package_loading.R
@@ -1,35 +1,45 @@
-### Master's Thesis
-### Jordan Heiman [others as needed]
+### rSPACE Modification                                                     ####
+### Author: Jordan Heiman
 ## Date: 2023-05-31
 
-## Function purpose: Load packages (check if installed and if not install them)
+## Source file purpose: Load packages (check if installed and if not install 
+##                      them first)
+
+#################################### Intro #####################################
+# Name: package_loading
+# Description:  This source file loads the packages needed for running the 
+#               rSPACE modification scripts. 
+# Associated publication:   Heiman, J.L., Tucker, J.M., Sells, S.N., Millspaugh, 
+#                         J.J., Schwartz, M.K., [in review]
+
+
+################################## Creation ####################################
+# Author: Jordan Heiman
+# Date Updated: 2024-10-17
+
+################################# Arguments ####################################
+# No arguments are provided to this function
+
+################################# Output #######################################
+# No outputs are given from this function
 
 ################################################################################
 ## Load required packages
-# Function from https://vbaliga.github.io/verify-that-r-packages-are-installed-and-loaded/
+# Script from:
+# https://vbaliga.github.io/verify-that-r-packages-are-installed-and-loaded/
 
 ## First specify the packages of interest
-packages <- c("raster", 
-              "tcltk2", 
-              "ggplot2", 
-              "RMark", 
-              "magrittr",
-              "terra",
-              "progress",
-              "dplyr",
-              "purrr",
-              "data.table",
-              "plyr")
+packages <- c("raster", "tcltk2", "ggplot2", "RMark", "magrittr", "terra",
+              "progress", "plyr", "dplyr", "purrr", "data.table")
 
 ## Now check each package, if it needs to be installed, install it, then load it
-package.check <- lapply(packages, FUN = function(x) {
-  if (!require(x, character.only = TRUE)) {
-    install.packages(x, dependencies = TRUE)
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+    }
     library(x, character.only = TRUE)
   }
-}
 )
 
-# Also need to install one package that is only on GitHub if downloading SWE data
-# devtools::install_github('marinosr/SNODASR')
-# library(SNODASR)
